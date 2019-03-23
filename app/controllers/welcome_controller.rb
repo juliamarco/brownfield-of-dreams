7f8d8505ff7f30f3c1a6d02a4425e047868cef2a
Julia Marco <marcootj@gmail.com>
2019-03-23 14:35:22 -0600

    Added facade rendering for welcome and tutorials controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
-    end
+    render locals: {
+      facade: TutorialsFacade.new(current_user)
+    }
+    # if params[:tag]
+    #   @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    # else
+    #   @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    # end
   end
 end
