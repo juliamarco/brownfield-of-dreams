d5fa2d026f1de0d953af2101d193b5a0115956e1
Julia Marco <marcootj@gmail.com>
2019-03-23 14:33:52 -0600

    Added test for visitor tutorials index page

---
+++ spec/features/visitor/visitor_cannot_see_classroom_tutorial_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  it 'cannot see classroom tutorials index' do
+    tutorial_1 = create(:tutorial, title: "How to bark", classroom: true)
+    tutorial_2 = create(:tutorial, title: "How to survive Turing")
+
+    visit root_path
+
+    expect(page).to have_content(tutorial_2.title)
+    expect(page).to_not have_content(tutorial_1.title)
+
+    visit tutorials_path
+
+    expect(page).to have_content(tutorial_2.title)
+    expect(page).to_not have_content(tutorial_1.title)
+  end
+
+  it 'cannot see a specific classroom tutorial' do
+    tutorial_1 = create(:tutorial, title: "How to bark", classroom: true)
+    tutorial_2 = create(:tutorial, title: "How to survive Turing")
+
+    visit tutorial_path(tutorial_1)
+
+    expect(page).to have_content("You have to register to access this tutorial")
+
+    visit tutorial_path(tutorial_2)
+
+    expect(page).to have_content(tutorial_2.title)
+  end
+end
