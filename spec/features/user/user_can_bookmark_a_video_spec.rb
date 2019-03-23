156c140a4207f0cb1c9188f69b5096791577f4d9
Julia Marco <marcootj@gmail.com>
2019-03-23 12:49:56 -0600

    Added feature test for users seeing a list of bookmarked videos

--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 describe 'A registered user' do
+  before :each do
+    @tutorial= create(:tutorial, title: "How to Tie Your Shoes", id: 2)
+    @video = create(:video, title: "The Bunny Ears Technique", tutorial: @tutorial, position: 2)
+    @user = create(:user)
+
+  end
   it 'can add videos to their bookmarks' do
-    tutorial= create(:tutorial, title: "How to Tie Your Shoes")
-    video = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial)
-    user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    visit tutorial_path(tutorial)
+    visit tutorial_path(@tutorial)
 
     expect {
       click_on 'Bookmark'
@@ -18,17 +21,44 @@ describe 'A registered user' do
   end
 
   it "can't add the same bookmark more than once" do
-    tutorial= create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
-    user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    visit tutorial_path(tutorial)
+    visit tutorial_path(@tutorial)
 
     click_on 'Bookmark'
     expect(page).to have_content("Bookmark added to your dashboard")
     click_on 'Bookmark'
     expect(page).to have_content("Already in your bookmarks")
+  end
+
+  it 'can see a list of bookmarked videos' do
+    video_2 = create(:video, title: "The Best Video", tutorial: @tutorial, position: 3)
+    tutorial_2 = create(:tutorial, title: "How to survive Turing", id: 1)
+    video_3 = create(:video, title: "Not easy", tutorial: tutorial_2, position: 1)
+
+    user_video_1 = create(:user_video, user: @user, video: @video)
+    user_video_2 = create(:user_video, user: @user, video: video_2)
+    user_video_3 = create(:user_video, user: @user, video: video_3)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    visit dashboard_path
+    expect(page).to have_content("Bookmarked Segments")
+
+    within "#bookmarks" do
+        within(first(".bookmark")) do
+          expect(page).to have_content(video_3.title)
+          expect(page).to_not have_content(@video.title)
+          expect(page).to_not have_content(video_2.title)
+      end
+
+      expect(page).to have_content(video_2.title)
+      expect(page).to have_content(@video.title)
+    end
+
+
+# And they should be organized by which tutorial they are a part of
+# And the videos should be ordered by their position
   end
 end
