87e2e1612b75c2a1adafa0be2cc402e275386c2a
Tim Allen <tnelsallen3@gmail.com>
2019-03-26 17:25:04 -0600

    Added Mailer test
    Co-authored by: Julia Marco <35079289+juliamarco@users.noreply.github.com>

--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe UserNotifierMailer, type: :mailer do
+  describe 'registration_email' do
+    let(:user) { mock_model User, first_name: "Lucas", email: "lucas@email.com", temporary_token: "12345678" }
+    let(:mail) { UserNotifierMailer.inform(user.email, user.temporary_token).deliver_now }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Account Activation')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['no-reply@brownfield.com'])
+    end
+
+    it 'assigns @confirmation_url' do
+      expect(mail.body.encoded)
+        .to include("/activation?token=#{user.temporary_token}")
+    end
+  end
 end
