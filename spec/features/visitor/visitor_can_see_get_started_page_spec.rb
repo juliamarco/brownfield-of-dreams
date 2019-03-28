8e6fb0dbe3b56ab6995632ae37b4fd94839010f7
Julia Marco <marcootj@gmail.com>
2019-03-28 15:34:49 -0600

    Added tests missing

---
+++ spec/features/visitor/visitor_can_see_get_started_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  it 'can see a get started page' do
+    visit '/get_started'
+
+    expect(page).to have_content("Browse tutorials from the homepage.")
+  end
+end
